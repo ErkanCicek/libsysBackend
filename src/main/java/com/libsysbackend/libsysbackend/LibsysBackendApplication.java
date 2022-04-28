34349ce530c0fe64f6229af1c4997f87df7b6464
Erkan Cicek <erkan.ec.cicek@gmail.com>
2022-04-28 14:05:41 +0200

    libsys Backend

---
+++ src/main/java/com/libsysbackend/libsysbackend/LibsysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.libsysbackend.libsysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libsysbackend/libsysbackend/LibsysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libsysbackend.libsysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
