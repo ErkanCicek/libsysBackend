5a6a6adb1c74e3cd862e07096f1d6b55e20cf551
bryan <bryan.bautista830@gmail.com>
2022-05-22 21:34:04 +0200

    Erkan, Lahoud och Bryan

--- src/main/java/com/libsysbackend/libsysbackend/entity/BorrowedBooks/BorrowedBooksController.java
+++ src/main/java/com/libsysbackend/libsysbackend/entity/BorrowedBooks/BorrowedBooksController.java
@@ -18,4 +18,20 @@ public class BorrowedBooksController {
                                       @RequestParam("SSN")String borrowerSSN){
         return this.borrowedBooksService.insertBorrowedBooks(returnDate, bookISBN, String.valueOf(bookId),borrowerSSN);
     }
+
+    @GetMapping("get/borrowedBooksBySSN")
+    public String getBorrowedBooksBySSN(@RequestParam("value")String ssn_in){
+        return this.borrowedBooksService.getBorrowedBooksBySSN(ssn_in);
+    }
+    @GetMapping("get/getAllBorrowedBooksBySSN")
+    public String getAllBorrowedBooksBySSN(@RequestParam("value")String SSN){
+        return this.borrowedBooksService.getAllBorrowedBooksBySSN(SSN);
+    }
+
+    /*
+    @GetMapping("get/allBorrowers")
+    public String getAllBorrowers(){
+        return this.borrowerService.getAllBorrowers();
+    }
+     */
 }
--- src/main/java/com/libsysbackend/libsysbackend/entity/BorrowedBooks/BorrowedBooksService.java
+++ src/main/java/com/libsysbackend/libsysbackend/entity/BorrowedBooks/BorrowedBooksService.java
@@ -8,4 +8,11 @@ public record BorrowedBooksService(BorrowedBooksDAO borrowedBooksDAO) {
     public String insertBorrowedBooks(String returnDate, String bookISBN, String bookId,String borrowerSSN){
         return borrowedBooksDAO.insertBorrowedBooks(returnDate, bookISBN, bookId, borrowerSSN);
     }
+    public String getBorrowedBooksBySSN(String ssn_in){
+        return this.borrowedBooksDAO.getBorrowedBooksBySSN(ssn_in);
+    }
+    public String getAllBorrowedBooksBySSN(String SSN){
+        return this.borrowedBooksDAO.getAllBorrowedBooksBySSN(SSN);
+    }
+
 }
