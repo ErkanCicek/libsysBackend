d9a69b99d1b1943ac525af35c45f4a0f24c0fe27
patrikselberg <patrik.selberg@edu.newton.se>
2022-06-01 14:51:27 +0200

    Testing done for BookLibDao

---
+++ src/test/java/DaoTests/BorrowerLibDaoTest.java
@@ -0,0 +1,28 @@
+package DaoTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BorrowerLibDaoTest {
+	
+	@Test
+	void getAllBorrowers() {
+	}
+	
+	@Test
+	void getBorrowerByBorrowerID() {
+	}
+	
+	@Test
+	void deleteBorrowerByID() {
+	}
+	
+	@Test
+	void addBorrower() {
+	}
+	
+	@Test
+	void addBorrowerCredentials() {
+	}
+}
